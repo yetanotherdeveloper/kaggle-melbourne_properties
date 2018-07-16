6857881626f5831e23843578f80b4dec19254279
Jacek Czaja <jacek.czaja@gmail.com>
2018-07-16 10:22:36 +0200

    - After updating data Property Count was solved. Added dropping entries without Price given

--- price-keras.py
+++ price-keras.py
@@ -13,47 +13,114 @@ from sklearn.preprocessing import StandardScaler
 import argparse
 import os
 
+def string2integer(ref,data,key):
+    if ref.has_key(key):
+        select = data[key]
+        integer_encoding = LabelEncoder().fit_transform(select)
+        data[key] = integer_encoding
+
+
+def guess_regionname(data,target_suburb,target_street):
+    ref = data['Regionname'].isnull()
+    # if there is entry within same subrn then return its region if available
+    for i in range(0,len(data)):
+        suburb = data['Suburb'][i]
+        if target_suburb == suburb and ref[i] == False:
+            return data['Regionname'][i]  
+        
+    # if there is entry within street name then return its RegionName
+    for i in range(0,len(data)):
+        street = data['Address'].values[i][data['Address'].values[i].find(" ")+1:]
+        if street == target_street and ref[i] == False:
+            return data['Regionname'][i]  
+    return "Unknown"
+
+
+def fill_region_name_up(data):
+    ref = data['Regionname'].isnull()
+    for i in range(0,len(ref)):
+        if ref[i] == True:
+            street = data['Address'].values[i][data['Address'].values[i].find(" ")+1:]
+            suburb = data['Suburb'][i]
+            data['Regionname'][i] = guess_regionname(data,suburb,street)
+    return
+
+
+def guess_property_count(data,target_suburb):
+    ref = data['Propertycount'].isnull()
+    for i in range(0,len(data)):
+        if target_suburb == data['Suburb'][i] and ref[i] == False:
+            return data['Propertycount'][i]
+    return 1 
+
+def fill_property_count_up(data):
+    """ Idea is to find property count from other entries of the same suburb"""
+    ref = data['Propertycount'].isnull()
+    for i in range(0,len(ref)):
+        if ref[i] == True:
+            data['Propertycount'][i] = guess_property_count(data,data['Suburb'][i]) 
+    return
+
 def load_and_preprocess_data(melbourne_file_path):
     melbourne_data = pd.read_csv(melbourne_file_path)
+    print(melbourne_data.isnull().sum()) # This is printing missing data
+    # Car: number of parking spots. NAN will be replaced wth zeros
+    melbourne_data['Car'].fillna(0,inplace=True)
+    # Bathroom: number of bathrroms. NAN will be replaced with ones
+    melbourne_data['Bathroom'].fillna(1,inplace=True)
+    # Try to make up the field of council area
+    melbourne_data['CouncilArea'].fillna("Unknown",inplace=True)
+    # Try to fill the missing RegionName
+    fill_region_name_up(melbourne_data)
+    # Try to fill the missing PropertyCount (properites count in suburb)
+    fill_property_count_up(melbourne_data)
+
+    melbourne_data = melbourne_data.dropna(axis=0, subset = ['Price'])  # Drop data that contains NAN in Price column
+    print(melbourne_data.isnull().sum()) # This is printing missing data
     melbourne_data = melbourne_data.dropna(axis=0)
+    ref = melbourne_data.tail().to_dict()
     # Replace string values of CouncilArea with onehott representation
-    council = melbourne_data['CouncilArea']
-    integer_encoding = LabelEncoder().fit_transform(council)
-    melbourne_data['CouncilArea'] = integer_encoding
+    string2integer(ref,melbourne_data,'CouncilArea')
     # Replace suburb name with integer encoding value
-    suburb =  melbourne_data['Suburb']
-    integer_encoding = LabelEncoder().fit_transform(suburb)
-    melbourne_data['Suburb'] = integer_encoding
+    string2integer(ref,melbourne_data,'Suburb')
     # Type of property
-    typedata =  melbourne_data['Type']
-    integer_encoding = LabelEncoder().fit_transform(typedata)
-    melbourne_data['Type'] = integer_encoding
+    string2integer(ref,melbourne_data,'Type')
     # enocode regionname 
-    region =  melbourne_data['Regionname']
-    integer_encoding = LabelEncoder().fit_transform(region)
-    melbourne_data['Regionname'] = integer_encoding
+    string2integer(ref,melbourne_data,'Regionname')
     # enocode SellerG 
-    seller =  melbourne_data['SellerG']
-    integer_encoding = LabelEncoder().fit_transform(seller)
-    melbourne_data['SellerG'] = integer_encoding
+    string2integer(ref,melbourne_data,'SellerG')
     # enocode Method 
-    method =  melbourne_data['Method']
-    integer_encoding = LabelEncoder().fit_transform(method)
-    melbourne_data['Method'] = integer_encoding
+    string2integer(ref,melbourne_data,'Method')
     # preprocess adress, remove apartmetn number , leaving just street
-    address =  melbourne_data['Address']
-    for i in range(0,len(address.values)):
-        address.values[i] = address.values[i][address.values[i].find(" ")+1:]
-    integer_encoding = LabelEncoder().fit_transform(address)
-    melbourne_data['Address'] = integer_encoding
+    if ref.has_key('Address'):
+        address =  melbourne_data['Address']
+        for i in range(0,len(address.values)):
+            address.values[i] = address.values[i][address.values[i].find(" ")+1:]
+        integer_encoding = LabelEncoder().fit_transform(address)
+        melbourne_data['Address'] = integer_encoding
 
     # Day + month*32 + 32*12*year
-    datas = melbourne_data['Date']
-    for i in range(0,len(datas.values)):
-        splits = datas.values[i].split('/')
-        datas.values[i] = int(splits[0]) + int(splits[1])*32 + int(splits[2])*32*12
+    if ref.has_key('Date'):
+        datas = melbourne_data['Date']
+        for i in range(0,len(datas.values)):
+            splits = datas.values[i].split('/')
+            datas.values[i] = int(splits[0]) + int(splits[1])*32 + int(splits[2])*32*12
     return melbourne_data
 
+def prepare_melbourne_dataset(melbourne_dataset_file):
+    melbourne_data = load_and_preprocess_data(melbourne_dataset_file)
+    # Ignored : Bedroom2, Latitude, Longtitude
+    input_features = ['Address','Bathroom','BuildingArea','Car','CouncilArea', 'Date', 'Distance', 'Landsize', 'Method', 'Postcode', 'Price', 'Propertycount', 'Regionname', 'Rooms', 'SellerG', 'Suburb', 'Type', 'YearBuilt']
+    return train_test_split(melbourne_data[input_features],melbourne_data.Price)
+
+def prepare_competition_dataset(train_data_file, test_data_file):
+    train_data = load_and_preprocess_data(train_data_file)
+    test_data = load_and_preprocess_data(test_data_file)
+    # Ignored:
+    # Alley -- Almost all NAN
+    input_features = ['MSSubClass', 'MSZoning', 'LotFrontage', 'LotArea', 'Street', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd', 'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'MasVnrArea', 'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinSF1', 'BsmtFinType2', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF', 'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'Bedroom', 'Kitchen', 'KitchenQual', 'TotRmsAbvGrd', 'Functional', 'Fireplaces', 'FireplaceQu', 'GarageType', 'GarageYrBlt', 'GarageFinish', 'GarageCars', 'GarageArea', 'GarageQual', 'GarageCond', 'PavedDrive', 'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'PoolQC', 'Fence', 'MiscFeature', 'MiscVal', 'MoSold', 'YrSold', 'SaleType', 'SaleCondition']
+
+    return train_data,train_data['SalePrice'],test_data,test_data['SalePrice'] 
 
 def parse_desc(model_desc):
     ''' Parse desc of model. Format is num of units in first layer, num of units in next layer..
@@ -139,16 +206,14 @@ def normalize_input(data,features):
         data[col] = scaler.transform(data[col])
 
 def train(model_name, model_desc, num_epochs, X, y):
-    melbourne_data = load_and_preprocess_data('./melb_data.csv')
-    #melbourne_data = load_and_preprocess_data('./train.csv')
 
     initial_epoch = 1
     if model_name == "" or model_name == "FFN": 
         model_name = "FFN"
-        melbourne_model = make_relu_model(model_name,model_desc,len(input_features))
+        melbourne_model = make_relu_model(model_name,model_desc,len(X.columns))
     elif model_name == "SNN":
         normalize_input(X,input_features)
-        melbourne_model = make_selu_model(model_name,model_desc,len(input_features))
+        melbourne_model = make_selu_model(model_name,model_desc,len(X.columns))
     else:
         tmpstr = model_name[:model_name.find("-val_loss")]
         initial_epoch = int(tmpstr[tmpstr.rfind("-")+1:])
@@ -175,7 +240,6 @@ def train(model_name, model_desc, num_epochs, X, y):
     predictions = melbourne_model.predict(X.values)
 
     print("MAE:",mean_absolute_error(predictions,y.values))
-
 # The Melbourne data has somemissing values (some houses for which some variables weren't recorded.)
 # We'll learn to handle missing values in a later tutorial.  
 # Your Iowa data doesn't have missing values in the predictors you use. 
@@ -195,7 +259,6 @@ def infer(model_name, X, y):
     print("MAE:",mean_absolute_error(predictions,y.values))
         
     scores = melbourne_model.evaluate(X.values,y.values,verbose=0)
-#    import pdb;pdb.set_trace()
     print("%s: %.2f" % (melbourne_model.metrics_names[0], scores))
 
 if __name__ == "__main__":
@@ -204,13 +267,16 @@ if __name__ == "__main__":
     parser.add_argument("--infer", help="Perform evaluation", action="store_true")
     parser.add_argument("--type", help="Type of Model to be used for training/inference", type=str, default="")
     parser.add_argument("--model", help="Model to be used for training/inference", type=str, default="20,20,1")
+    parser.add_argument("--dataset", help="Data Set for training/inference", type=str, default="melb")
     parser.add_argument("--num_epochs", help="Number of epochs to perform", type=int, default=10)
     args = parser.parse_args()
 
-    melbourne_data = load_and_preprocess_data('./melb_data.csv')
-    input_features = ['Address','Bathroom','Bedroom2','BuildingArea','Car','CouncilArea', 'Date', 'Distance', 'Landsize', 'Lattitude', 'Longtitude', 'Method', 'Postcode', 'Price', 'Propertycount', 'Regionname', 'Rooms', 'SellerG', 'Suburb', 'Type', 'YearBuilt']
-    #input_features = ['Bathroom','Bedroom2','BuildingArea','Car','Distance', 'Landsize', 'Lattitude', 'Longtitude', 'Postcode', 'Price', 'Propertycount', 'Rooms' ]
-    trainX, testX, trainY, testY = train_test_split(melbourne_data[input_features],melbourne_data.Price)
+    if args.dataset == 'melb':
+        trainX, testX, trainY, testY = prepare_melbourne_dataset('./Melbourne_housing_FULL.csv')
+    elif args.dataset == 'comp':
+        trainX, testX, trainY, testY = prepare_competition_dataset('./train.csv','./test.csv')
+    else:
+        printf("Invalid value of dataset. Accepted values: 'comp' and 'melb'");
 
     if args.train == True:    
         train(args.type,args.model,args.num_epochs,trainX, trainY)
